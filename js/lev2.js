760f93003fec9b130ef136ee0f60cd3772a8887f
Astriadi Setrawandana <astriadi@outlook.com>
2019-05-06 23:50:24 +0700

    first upload

---
+++ js/lev2.js
@@ -0,0 +1,43 @@
+function lev2(a, b) {
+  var a = document.getElementById("formlev").elements[0].value;
+  var b = document.getElementById("formlev").elements[1].value;
+
+  var len1 = a.length;
+  var len2 = b.length;
+  
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  var matrix = [];
+
+  // increment along the first column of each row
+  var i;
+  for (i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  var j;
+  for (j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) == a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          Math.min(
+            matrix[i][j - 1] + 1, // insertion
+            matrix[i - 1][j] + 1
+          )
+        ); // deletion
+      }
+    }
+  }
+
+  document.getElementById("tes").innerHTML = ((Math.max(len1, len2) - matrix[b.length][a.length])/Math.max(len1, len2)*100).toFixed(2) + '%';
+}
